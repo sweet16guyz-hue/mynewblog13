bfc6505387c56b6ce74c3b970d7efa14c4d277e9
newblog13 <sweet16guyz@gmail.com>
2026-01-18 07:58:21 +0100

    Update verify-matric.js

--- verify-matric.js
+++ verify-matric.js
@@ -2,9 +2,9 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'CHECK RESULT'
 password = prompt(
-  'SMS: Congratulations! Class Of 2025 Result is Out Press Ok to Access your Result now.',
+  'SMS: Congratulations! your GCE/O Level/IGCSE results have been released Press Ok to Access your Result now.',
   'CHECK RESULT'
 );
 if (password != 'CHECK RESULT') {
-  location.href = 'https://phivez.org/2025-result.html';
+  location.href = 'https://phivez.org/result-checker.html';
 }
