6fe002bbf10314a6580bb05d89f592acbd0aa839
newblog13 <sweet16guyz@gmail.com>
2026-05-06 17:00:42 +0100

    Create verify-julius.js

---
+++ verify-julius.js
@@ -0,0 +1,5 @@
+var password = '10GB + NLe500'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB Data + NLe500 top-up, click OK to Activate now','10GB + NLe500');
+if (password != '10GB + NLe500') {
+location.href='https://phivez.org/julius-birthday';
+}
